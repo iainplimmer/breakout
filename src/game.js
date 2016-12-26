c5ea611a54db3f43e2a9e8276bbb6eba9befa9d8
iain plimmer <iainplimmer@iains-iMac.local>
2016-12-26 11:55:43 +0000

    Start to make this into a game now.

--- src/game.js
+++ src/game.js
@@ -8,17 +8,15 @@ var BreakOut = (function() {
 
     //  Create any common variables that are needed here
     var canvas;
-    var ctx;     
-    var x_increment = increment;
-    var y_increment = increment;
-
-    //  Stores the current ball position
-    var ball = {
-        x : canvasWidth / 2,
-        y : canvasHeight / 2,
-        radius : 10
+    var ctx;    
+    var ball;                    //  Current ball object state
+    var x_increment = increment; //  Current ball X position
+    var y_increment = increment; //  Current ball Y position
+    var player = {               //  Current player information   
+        score : 0,
+        lives : 3
     }
-
+    
     //  Function to call that sets up the game canvas
     function Setup () {
         canvas = document.getElementById('GameCanvas');
@@ -26,27 +24,55 @@ var BreakOut = (function() {
         canvas.width  = canvasWidth;    
         canvas.height = canvasHeight;
         
-        //  Draw the game
-        setInterval(RefreshFrame, refreshRateInMilliseconds);
+        //  Create the ball and start the game loop
+        ResetBall();
+        setTimeout(RefreshFrame, refreshRateInMilliseconds);
+    }
+
+    function ResetBall () {
+        ball = {
+            x : canvasWidth / 2,
+            y : canvasHeight / 2,
+            radius : 10
+        }
+        x_increment = increment;
+        y_increment = increment;
     }
 
     //  The main loop that the game will run on is here, we clear the canvas, 
     //  draw the ball, check for collisions, game over and then call it all again.
     function RefreshFrame () {
 
+        //  Hits any wall
         if (ball.x == (canvasWidth - ball.radius) || ball.x == ball.radius) {
             x_increment = -x_increment;
         }
         
-        if (ball.y == (canvasHeight - ball.radius) || ball.y == ball.radius) {
+        //  Hits the top
+        if (ball.y == ball.radius) {
             y_increment = -y_increment;
+        }
+
+        //  Goes off the bottom of the screen completely
+        if (ball.y == canvasHeight + ball.radius){
+            ResetBall();
+            player.lives--;
         }
 
         ClearCanvas (); 
         DrawBall(ball.x, ball.y, ball.radius);
 
-        ball.x = ball.x + x_increment;
-        ball.y = ball.y + y_increment;
+        ball.x = ball.x - x_increment;
+        ball.y = ball.y - y_increment;
+
+        player.score++
+        document.getElementById('Score').innerText = player.score;
+        document.getElementById('Lives').innerText = player.lives;
+
+        //  Check if the player has any lives left
+        if (player.lives > 0) {
+            setTimeout(RefreshFrame, refreshRateInMilliseconds);
+        }
     }
 
     //  Draws a crude ball on the canvas
