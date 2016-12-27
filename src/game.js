acb548a784142168d6b7e55187405d989773d147
iain plimmer <iainplimmer@iains-iMac.local>
2016-12-27 12:15:00 +0000

    More tidying up.

--- src/game.js
+++ src/game.js
@@ -2,8 +2,8 @@ var BreakOut = (function() {
 
     //  Set our constants first for the game rate and window size......
     const refreshRateInMilliseconds = 5;
-    const canvasWidth = 500;
-    const canvasHeight = 300;
+    const canvasWidth = 400;
+    const canvasHeight = 200;
     const defaultBrickHeight = 20;
     const leftKey = 122;    // Z
     const righttKey = 109;  // M
@@ -12,9 +12,9 @@ var BreakOut = (function() {
     //  named in uppercase so that they are more visible, i'm not just passing around functions at the moment
     var CANVAS;
     var CTX;    
-    var BALL = {};           //  Current ball object state
-    var PLAYER = {};         //  Current player/paddle information  
-    var WALL = [];           //  Stores the information about the wall 
+    var BALL;           //  Current ball object state
+    var PLAYER;         //  Current player/paddle information  
+    var WALL ;           //  Stores the information about the wall 
          
     //  Function to call that sets up the game canvas, sets up the canvas,
     //  the event listener and then starts the ball and the game
@@ -23,29 +23,21 @@ var BreakOut = (function() {
         CTX = CANVAS.getContext("2d");
         CANVAS.width  = canvasWidth;    
         CANVAS.height = canvasHeight;
-
-        //  Let's listen for when the player presses keys
-        document.addEventListener('keypress', function (e) {
-            var key = e.which || e.keyCode;
-            if (key === leftKey && PLAYER.paddleLeft > 0) { 
-                PLAYER.paddleLeft = PLAYER.paddleLeft-20; 
-            }
-            else if (key === righttKey && (PLAYER.paddleLeft+PLAYER.paddleWidth) < canvasWidth) { 
-                PLAYER.paddleLeft = PLAYER.paddleLeft+20; 
-            }
-        });
         
-        //  Create the ball and start the game loop
-        ResetWall();
-        DrawWall();
-        ResetPlayer();
-        ResetBall();
-        RefreshFrame();
+        //Start the game loop
+        Play();
     }
 
     //  The main loop that the game will run on is here, we clear the canvas, 
     //  draw the ball, check for collisions, game over and then call it all again.
-    function RefreshFrame () {
+    function Play () {
+
+        //  Create the ball, player and wall if they are not setup
+        if (!WALL && !PLAYER && !BALL) {        
+            WALL = ResetWall();
+            PLAYER = ResetPlayer();
+            BALL = ResetBall();
+        }
 
         //  Hits any side wall
         if (BALL.x == (canvasWidth - BALL.radius) || BALL.x == BALL.radius) {
@@ -58,13 +50,14 @@ var BreakOut = (function() {
         }
 
         //  Collides with any of the blocks on the screen
+        var wallStanding = false;
         WALL.forEach(function(br, ind){
             
             //  What's the balls current Y in relation to the brick and radius?
             var brickbottomY = br.y + br.height + BALL.radius;
             var bricktopY = br.y + BALL.radius;
 
-            //  Has the ball hit the bottom of the brick?
+            //  Has the ball hit the bottom?
             if (br.draw 
                 && (BALL.y == brickbottomY)
                 && BALL.x < br.rightx
@@ -74,14 +67,19 @@ var BreakOut = (function() {
                 br.draw = false;
                 //throw new Error('Collision detection!');    
             } 
+
+            //  Set to true if even on brick is standing
+            if (br.draw) {
+                wallStanding = true;
+            }
         });
 
         //  Goes off the bottom of the screen completely. Decrement the number of lives
         //  and reset the ball to start the player again
         if (BALL.y == canvasHeight + BALL.radius){
             PLAYER.lives--;
-            ResetBall();
-        }
+            BALL = ResetBall();
+        }        
 
         //  Hits the paddle (want to add a hit left, right and middle modifier here)
         if (BALL.y == canvasHeight - PLAYER.paddleHeight
@@ -106,9 +104,9 @@ var BreakOut = (function() {
         document.getElementById('Score').innerText = PLAYER.score;
         document.getElementById('Lives').innerText = PLAYER.lives;      
 
-        //  Check if the player has any lives left
-        if (PLAYER.lives > 0) {
-            setTimeout(RefreshFrame, refreshRateInMilliseconds);
+        //  Check if the player has any lives left and that the wall is still standing
+        if (PLAYER.lives > 0 && wallStanding) {
+            setTimeout(Play, refreshRateInMilliseconds);
         }
     }
 
@@ -149,7 +147,8 @@ var BreakOut = (function() {
     //  Creates the array of objects that form the wall
     function ResetWall () {    
         const defaultBricksPerRow = 5;
-        const defaultBrickRows = 3;          
+        const defaultBrickRows = 3;       
+        var wall = [];   
         var brickWidth = canvasWidth/defaultBricksPerRow;
         for(var row=0; row < 3; row++) {
             for(var col=0; col < canvasWidth; col+=brickWidth) {
@@ -162,13 +161,28 @@ var BreakOut = (function() {
                     rightx : (col)+brickWidth,
                     draw : true
                 }
-                WALL.push(brick);
+                wall.push(brick);
             }
         }
+        return wall;
     }
 
+    //  Let's setup the player's controls and return a player object for use in the game
     function ResetPlayer () {
-        PLAYER = {      //  Current player/paddle information   
+
+        //  Let's listen for when the player presses keys
+        document.addEventListener('keypress', function (e) {
+            var key = e.which || e.keyCode;
+            if (key === leftKey && PLAYER.paddleLeft > 0) { 
+                PLAYER.paddleLeft = PLAYER.paddleLeft-40; 
+            }
+            else if (key === righttKey && (PLAYER.paddleLeft+PLAYER.paddleWidth) < canvasWidth) { 
+                PLAYER.paddleLeft = PLAYER.paddleLeft+40; 
+            }
+        });
+
+        //  Return the current player/paddle information   
+        return {      
             score : 0,
             lives : 3,
             paddleHeight : 5,
@@ -177,8 +191,9 @@ var BreakOut = (function() {
         };
     }
 
+    //  Puts the ball into a starting position and sets it's movement increments
     function ResetBall () {
-        BALL = {
+        return {
             x : canvasWidth / 2,    
             y : canvasHeight / 2,
             radius : 5,
